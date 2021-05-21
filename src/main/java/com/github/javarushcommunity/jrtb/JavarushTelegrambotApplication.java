d7c676bedb618341fbddd2c30ba1f7fe8fc7bda5
MargaritaGl <margarita.glushko@gmail.com>
2021-05-21 15:25:31 +0300

    JRTB-2 Added telegram bot to the project

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegrambotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegrambotApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavarushTelegrambotApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JavarushTelegrambotApplication.class, args);
 	}
 
